d1a147925992b0c5671f692fa3c8f350507e26c2
eb.developer87@gmail.com <eb.developer87@gmail.com>
2025-07-07 21:38:15 +0200

    feat: add components

---
+++ src/app/about-me/about-me.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-about-me',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-me.html',
+  styleUrls: ['./about-me.scss']
+})
+export class AboutMeComponent {}
+
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me';
+import { ContactComponent } from './contact/contact';
+import { HeaderComponant } from './shared/header/header';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+   standalone: true,
+  imports: [RouterOutlet, AboutMeComponent, ContactComponent, HeaderComponant ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
