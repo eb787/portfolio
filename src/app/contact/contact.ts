3b3e4aee223ee9b7a3f32a46b4acc037758f69b5
eb.developer87@gmail.com <eb.developer87@gmail.com>
2025-07-19 15:44:03 +0200

    feat: section contact and new links

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,12 +1,57 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
 export class ContactComponent {
 
+  http = inject(HttpClient);
+
+  contactData ={
+    name: '',
+    email: '',
+    message: '',
+
+  }
+
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://contact@eric-braun.net/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
+  }
 }
