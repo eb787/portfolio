4713aa94b1e9b8b9d66cf44f651c0609b73a801f
eb.developer87@gmail.com <eb.developer87@gmail.com>
2025-07-20 17:23:09 +0200

    feat: footer added

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,13 +7,14 @@ import { ProjectsComponent } from './projects/projects';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold';
 import { MySkillsComponent } from './my-skills/my-skills';
 import { ThoughtsColleagues } from './thoughts-colleagues/thoughts-colleagues';
+import { FooterComponent } from './shared/footer/footer';
 
 
 
 @Component({
   selector: 'app-root',
    standalone: true,
-  imports: [RouterOutlet, AboutMeComponent, ContactComponent, HeaderComponant, ProjectsComponent, AboutMeComponent, AboveTheFoldComponent, MySkillsComponent, ThoughtsColleagues],
+  imports: [RouterOutlet, AboutMeComponent, ContactComponent, HeaderComponant, ProjectsComponent, AboutMeComponent, AboveTheFoldComponent, MySkillsComponent, ThoughtsColleagues, FooterComponent ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
