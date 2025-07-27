873327c98fcdc169d466e9a97e2fec4f8c79e70c
eb.developer87@gmail.com <eb.developer87@gmail.com>
2025-07-27 19:49:59 +0200

    feat: new responsive above-the-fold and translate projects section

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './projects.html',
   styleUrl: './projects.scss'
 })
 export class ProjectsComponent {
+ currentLang: string;
 
+  constructor(private translate: TranslateService) {
+    this.currentLang = this.translate.currentLang || this.translate.getDefaultLang();
+    this.translate.onLangChange.subscribe((event) => {
+      this.currentLang = event.lang;
+    });
+  }
 }
