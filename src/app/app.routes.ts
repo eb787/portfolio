194364d5544a80c4d39b2f6e2611341459f3b9f2
eb.developer87@gmail.com <eb.developer87@gmail.com>
2025-09-08 21:25:24 +0200

    refector: links and padding section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,23 @@
-import {  Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponant } from './home/home';
 import { PrivacyPolicyComponant } from './privacy-policy/privacy-policy';
 import { LegalNoticeComponent } from './legal-notice/legal-notice';
 
-
-
 export const routes: Routes = [
-    { path: '', component: HomeComponant },
+  { path: '', component: HomeComponant },
   { path: 'legal-notice', component: LegalNoticeComponent },
   { path: 'privacy-policy', component: PrivacyPolicyComponant }
 ];
 
-
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled',  // ← hier wird das automatische Scrollen aktiviert
+      anchorScrolling: 'enabled'             // optional, falls du #anker-Links hast
+    })
+  ],
+  exports: [RouterModule]
+})
 export class AppRoutingModule { }
+
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { filter } from 'rxjs/operators';
@@ -13,16 +13,28 @@ import { filter } from 'rxjs/operators';
 export class App {
   protected title = 'portfolio';
 
-  constructor(private translate: TranslateService, private router: Router) {
-    this.translate.setDefaultLang('de');
+  constructor(
+    private translate: TranslateService,
+    private router: Router,
+    private ngZone: NgZone
+  ) {
 
+    this.translate.setDefaultLang('de');
     const browserLang = this.translate.getBrowserLang();
     this.translate.use(browserLang?.match(/de|en/) ? browserLang : 'de');
 
     this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe(() => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+      .subscribe((event: NavigationEnd) => {
+        const url = event.urlAfterRedirects;
+
+        if (!url.includes('#')) {
+          this.ngZone.runOutsideAngular(() => {
+            setTimeout(() => {
+              window.scrollTo({ top: 0, behavior: 'smooth' });
+            }, 100);
+          });
+        }
       });
   }
 
